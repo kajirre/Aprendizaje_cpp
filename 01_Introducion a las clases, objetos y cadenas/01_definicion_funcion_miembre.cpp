2e2bda73252d3242b5045f7b8979289f8df4aaa2
jhonatan <jhonatan_20.25@outlook.com>
2026-02-04 17:22:34 -0500

    nuevo tema

---
+++ 01_Introducion a las clases, objetos y cadenas/01_definicion_funcion_miembre.cpp
@@ -0,0 +1,31 @@
+// Difine la clase LibroCalificaciones con una funcion miembro que recibe un parametro
+
+#include <iostream>
+#include <string>   // ----> el programa usa la clase string estandar de c++
+using namespace std;
+
+// Definicion de la clase LibroCalificaciones
+class LibroCalificaciones{
+    public:
+        //la funcion quie muestra el mensaje de bienvenida para el usuario de LibroCalificaciones
+        void mostrarMensaje(string nombreCurso) const{
+            cout << "Bienvenido al libro de calificaciones para " << nombreCurso << "!" << endl;
+        } // fin fucnion mostrar mensaje
+};
+
+
+int main(){
+    string nombreDelCurso; 
+    LibroCalificaciones milibroCalificaciones;
+
+    // pide y recibe el nombre del curso como entrada
+    cout << "Escriba el nombre del curso: " << endl;
+    getline(cin, nombreDelCurso); // lee el nombre del curso con espacios en blanco
+    cout << endl; // imprime una linea en blanco
+
+    // llama a la funcion mostrarMensaje de miLibroCalificaciones
+    // y pasa nombreDelCurso como argumento
+
+    milibroCalificaciones.mostrarMensaje(nombreDelCurso);
+    return 0;
+}
